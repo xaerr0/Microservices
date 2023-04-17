7c8649ad57f56caaf3e814ab4d4d6571077df459
Danny Brown <xaerr00@gmail.com>
2023-04-16 22:02:44 -0700

    first commit

---
+++ src/test/java/co/codingnomads/spring/managementservice/ManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.codingnomads.spring.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
